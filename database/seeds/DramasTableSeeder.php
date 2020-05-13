8aac28da16e3002f5b23447c01faaefeac41c878
”sojungyou” <yousj0226@hotmail.co.jp>
2020-05-13 17:45:26 +0900

    data

--- database/seeds/DramasTableSeeder.php
+++ database/seeds/DramasTableSeeder.php
@@ -22,6 +22,17 @@ class DramasTableSeeder extends Seeder
             'cast' => 'ヒョンビン / ソン・イェジン /  ソ・ジヘ  / キム・ジョンヒョン',
             'video_path' => 'https://www.youtube.com/embed/zPe5AdGCszc'
         ]);
+        DB::table('dramas')->insert([
+            'title' => 'プリズン・ブレイク',
+            'subtitle' => 'Prison Break ',
+            'image_path' => 'https://i.gyazo.com/046f8b17cc1cfec07a10be0e119d450d.png',
+            'story' => 'マイケルは何不自由なく暮らす優秀な建築設計士。しかしある日、兄のリンカーンが副大統領の兄弟殺しの罪で逮捕され、死刑判決を受けたことから人生が一変する。異例のスピード判決、無罪を主張し続ける兄……得体の知れない陰謀に巻き込まれた兄を合法的に助けることは不可能だと知ったマイケルは、武装強盗を犯し自ら犯罪者となって兄と同じ刑務所へ入る。兄とともに脱出するために……。',
+            'genre' => 'アクションサスペンスドラマ',
+            'releaseDate' => '2005年8月29日',
+            'country' => 'アメリカ',
+            'cast' => 'ウェントワース・ミラードミニク・パーセルピーター・ストーメアロバート・ネッパー',
+            'video_path' => 'https://www.youtube.com/embed/AL9zLctDJaU	'
+        ]);
     }
 }
 
